a5f389ada4843771a92cf84de103cdd2aec768e3
David <75451291+dulkoss@users.noreply.github.com>
2024-01-03 11:57:11 +0100

    custom prompts for missing input

--- src/Commands/ResendMailCommand.php
+++ src/Commands/ResendMailCommand.php
@@ -7,6 +7,8 @@ use Illuminate\Contracts\Console\PromptsForMissingInput;
 use Vormkracht10\Mails\Jobs\ResendMailJob;
 use Vormkracht10\Mails\Models\Mail;
 
+use function Laravel\Prompts\{text, info};
+
 class ResendMailCommand extends Command implements PromptsForMissingInput
 {
     public $signature = 'mail:resend {uuid} {to?*} {--cc=*} {--bcc=*}';
@@ -25,15 +27,45 @@ class ResendMailCommand extends Command implements PromptsForMissingInput
             return Command::FAILURE;
         }
 
-        ResendMailJob::dispatch($mail,
-            $this->argument('to') ?: $mail->to,
-            $this->option('cc') ?: $mail->cc ?? [],
-            $this->option('bcc') ?: $mail->bcc ?? [],
-        );
+        info('For the next prompts you can input multiple email addresses by separating them with a comma.');
+
+        [$to, $cc, $bcc] = $this->promptEmailInputs($mail);
+
+        ResendMailJob::dispatch($mail, $to, $cc, $bcc);
 
         $this->comment('All done');
 
         return self::SUCCESS;
+    }
+
+    protected function promptEmailInputs(Mail $mail): array
+    {
+        $to = $this->argument('to') ?: explode(',', text(
+            label: 'What email address do you want to send the mail to?',
+            placeholder: 'test@example.com',
+        ));
+
+        $cc = $this->option('cc') ?: explode(',', text(
+            label: 'What email address should be included in the cc?',
+            placeholder: 'test@example.com',
+        ));
+
+        $bcc = $this->option('bcc') ?: explode(',', text(
+            label: 'What email address should be included in the bcc?',
+            placeholder: 'test@example.com',
+        ));
+
+        $inputs = [
+            [&$to, $mail->to],
+            [&$cc, $mail->cc ?? []],
+            [&$bcc, $mail->bcc ?? []],
+        ];
+
+        foreach ($inputs as [&$array, $default]) {
+            $array = array_filter(array_map(fn ($email) => trim($email), $array)) ?: $default;
+        }
+
+        return [$to, $cc, $bcc];
     }
 
     protected function promptForMissingArgumentsUsing()
