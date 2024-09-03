377197bd4bc84568028e66e2b6073e4bf9fc1e1b
Mark van Eijk <mark@vaneijk.co>
2024-09-03 09:53:56 +0200

    wip

--- routes/webhooks.php
+++ routes/webhooks.php
@@ -5,7 +5,10 @@ use Illuminate\Support\Facades\Route;
 use Vormkracht10\Mails\Controllers\MailgunWebhookController;
 use Vormkracht10\Mails\Controllers\PostmarkWebhookController;
 
-Route::middleware(['api', 'signed'])->withoutMiddleware(VerifyCsrfToken::class)->group(['prefix' => config('mails.webhooks.routes.prefix')], function () {
-    Route::post('mailgun/{type}', MailgunWebhookController::class)->name('mails.webhooks.mailgun');
-    Route::post('postmark', PostmarkWebhookController::class)->name('mails.webhooks.postmark');
-});
+Route::middleware(['api', 'signed'])
+    ->withoutMiddleware(VerifyCsrfToken::class)
+    ->prefix(config('mails.webhooks.routes.prefix'))
+    ->group(function () {
+        Route::post('mailgun/{type}', MailgunWebhookController::class)->name('mails.webhooks.mailgun');
+        Route::post('postmark', PostmarkWebhookController::class)->name('mails.webhooks.postmark');
+    });
