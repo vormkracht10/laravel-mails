11111d01d9d9a33e62909897327ae41c1bb7083a
Manoj Hortulanus <mdhortulanus@gmail.com>
2025-02-28 10:48:45 +0100

    Add tags to LogMail

--- src/Actions/LogMail.php
+++ src/Actions/LogMail.php
@@ -71,6 +71,9 @@ class LogMail
             'bcc' => $this->getAddressesValue($event->message->getBcc()),
             'html' => $event->message->getHtmlBody(),
             'text' => $event->message->getTextBody(),
+            'tags' => collect($event->message->getHeaders()->all('X-tag'))
+            ->map(fn ($tag) => $tag->getValue())
+            ->toArray(),
         ];
     }
 
