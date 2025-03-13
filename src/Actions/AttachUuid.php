41fa5973e75395d342b5a5a777590706672d20f8
Mark van Eijk <mark@vaneijk.co>
2025-03-13 11:31:11 +0100

    AttachUUID only when mail provider has a driver

--- src/Actions/AttachUuid.php
+++ src/Actions/AttachUuid.php
@@ -13,15 +13,42 @@ class AttachUuid
 
     public function handle(MessageSending $event): void
     {
+        $provider = $this->getProvider($event);
+
+        if($this->shouldTrackMails($provider)) {
+            return;
+        }
+
         $uuid = Str::uuid()->toString();
 
         $event->message->getHeaders()->addTextHeader(config('mails.headers.uuid'), $uuid);
 
-        $event = MailProvider::with($this->getProvider($event))->attachUuidToMail($event, $uuid);
+        $event = MailProvider::with($provider)->attachUuidToMail($event, $uuid);
     }
 
     public function getProvider(MessageSending $event): string
     {
         return config('mail.mailers.'.$event->data['mailer'].'.transport') ?? $event->data['mailer'];
+    }
+
+    public function shouldTrackMails(string $provider): bool
+    {
+        return $this->trackingEnabled() &&
+            $this->driverExistsForProvider($provider);
+    }
+
+    public function driverExistsForProvider(string $provider): bool
+    {
+        return class_exists('Vormkracht10\\Mails\\Drivers\\'.ucfirst($provider).'Driver');
+    }
+
+    public function trackingEnabled(): bool
+    {
+        return (bool) config('mails.logging.tracking.bounces') === true ||
+            (bool) config('mails.logging.tracking.clicks') === true ||
+            (bool) config('mails.logging.tracking.complaints') === true ||
+            (bool) config('mails.logging.tracking.deliveries') === true ||
+            (bool) config('mails.logging.tracking.opens') === true ||
+            (bool) config('mails.logging.tracking.unsubscribes') === true;
     }
 }
