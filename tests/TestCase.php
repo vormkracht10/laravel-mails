835c4c3e082c630f97e5e2fa6edd0718108dd837
danielhe4rt <danielhe4rt@gmail.com>
2025-07-21 20:24:43 -0300

    fix: test suite models bootstrap

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,25 +2,29 @@
 
 namespace Backstage\Mails\Tests;
 
+use Backstage\Mails\MailsServiceProvider;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Filesystem\Filesystem;
 use NotificationChannels\Discord\DiscordServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
-use SplFileInfo;
-use Backstage\Mails\MailsServiceProvider;
 
 class TestCase extends Orchestra
 {
+
+    protected static array $migrations = [];
+
     protected function setUp(): void
     {
         parent::setUp();
 
         Factory::guessFactoryNamesUsing(
-            fn (string $modelName) => 'Backstage\\Mails\\Database\\Factories\\'.class_basename($modelName).'Factory'
+            fn(string $modelName) => 'Backstage\\Mails\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
         );
+
+        $this->loadMigrations();
     }
 
-    protected function getPackageProviders($app)
+    protected function getPackageProviders($app): array
     {
         return [
             DiscordServiceProvider::class,
@@ -28,14 +32,43 @@ class TestCase extends Orchestra
         ];
     }
 
+    /**
+     * Set up the environment for testing.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
     public function getEnvironmentSetUp($app)
     {
-        config([
-            'database.default' => 'testing',
-            'queue.default' => 'sync',
+        $app['config']->set('database.default', 'sqlite');
+        $app['config']->set('database.connections.sqlite', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
         ]);
 
-        collect(app(Filesystem::class)->files(__DIR__.'/../database/migrations/'))
-            ->map(fn (SplFileInfo $file) => require __DIR__.'/../database/migrations/'.$file->getBasename());
+        $app['config']->set('queue.default', 'sync');
+    }
+
+    /**
+     * Load and run migrations from stub files
+     */
+    protected function loadMigrations(): void
+    {
+        $filesystem = new Filesystem();
+        $migrationFiles = $filesystem->files(__DIR__ . '/../database/migrations/');
+
+        // Sorting to ensure migrations run in the correct order
+        usort($migrationFiles, function ($a, $b) {
+            return strcmp($a->getFilename(), $b->getFilename());
+        });
+
+        foreach ($migrationFiles as $file) {
+            // Skip if not a stub file
+            if ($file->getExtension() !== 'stub') {
+                continue;
+            }
+
+            $migration = include $file->getPathname();
+            $migration->up();
+        }
     }
 }
